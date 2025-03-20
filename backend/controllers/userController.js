2b9c0f6c03e580d918b026c10233410fb64b179d
Arun Kumar <mymailarun32@gmail.com>
2025-03-20 09:21:38 +0530

    feat(userController): add OTP generation and Twilio integration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,13 @@
-export const signUp = async (req, res) => {
+import { sendOTP } from "../services/otpService.js";
+
+export const sendOtp = async (req, res) => {
   try {
-    res.send("jkskjsjkfjfjj");
+    const { mobile } = req.body;
+    if (!mobile) {
+      return res.status(400).json({ message: "Mobile Number is required" });
+    }
+    const response = await sendOTP(mobile);
+    res.status((response.success ? 200 : 500).json(response));
   } catch (error) {
     console.log(error);
   }

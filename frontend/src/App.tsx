6e1b9a83c69323d38dc0bd1c872bb55fd3494a16
Arun Kumar <mymailarun32@gmail.com>
2025-03-20 17:12:32 +0530

    feat(App): wrap query client provider in the app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router"
 import Login from "./pages/login/login"
 import Signup from "./pages/signup/signup"
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 

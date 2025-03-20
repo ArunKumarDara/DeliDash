5a23e854427bad09b45f69cd9aa76a36ce12d06d
Arun Kumar <mymailarun32@gmail.com>
2025-03-20 11:56:02 +0530

    feat(server): add middleware for restaurants

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { dbConfig } from "./config/dbConfig.js";
 import userRouter from "./router/userRouter.js";
+import restaurantRouter from "./router/restaurantRouter.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const port = process.env.PORT || 5000;
 
   app.use(express.json());
   app.use("/api/v1/users", userRouter);
+  app.use("/api/v1/restaurants", restaurantRouter);
 
   app.get("/", (req, res) => {
     res.send("Hello, world!");

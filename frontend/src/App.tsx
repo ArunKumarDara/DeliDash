3e0b4291d3c92b377b9d56db1a2d71d5543e0950
Arun Kumar <mymailarun32@gmail.com>
2025-03-20 16:02:05 +0530

    feat(App): create route for signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router"
 import Login from "./pages/login/login"
+import Signup from "./pages/signup/signup"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   )

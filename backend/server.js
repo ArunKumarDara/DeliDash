2b25fd2526273c55eedfb3efab51ff041bc2a732
Arun Kumar <mymailarun32@gmail.com>
2025-03-21 09:39:34 +0530

    feat(server): added `cookie-parser` middleware
    - Integrated `cookie-parser` to handle cookies in requests.
    - Ensured CORS setup allows credentials for secure cross-origin requests.
    - Wrapped database initialization in an async function before starting the server.
    - Defined routes for user and restaurant APIs.
    - Server listens on a configurable port with a startup message.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import { dbConfig } from "./config/dbConfig.js";
 import userRouter from "./router/userRouter.js";
 import restaurantRouter from "./router/restaurantRouter.js";
@@ -13,6 +15,13 @@ const port = process.env.PORT || 5000;
   await dbConfig();
 
   app.use(express.json());
+  app.use(cookieParser());
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
   app.use("/api/v1/users", userRouter);
   app.use("/api/v1/restaurants", restaurantRouter);
 

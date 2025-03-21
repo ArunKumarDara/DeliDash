1b294ecb084d3614b840fa028c80db330494c9b0
Arun Kumar <mymailarun32@gmail.com>
2025-03-21 09:37:32 +0530

    feat(signup): add signup functionality with validation
    - Implemented a signup form using React Hook Form with `zod` validation.
    - Integrated `useMutation` from React Query for handling user signup requests.
    - Used ShadCN UI components for input fields, labels, and buttons.
    - Added an `InputOTP` component for a 6-digit numeric MPIN entry.
    - Included real-time error messages and a loading state during form submission.
    - Redirected users to the login page on successful signup.
    - Displayed toast notifications for success and error messages.

--- frontend/src/components/signup-form.tsx
+++ frontend/src/components/signup-form.tsx
@@ -1,72 +1,141 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
     InputOTP,
     InputOTPGroup,
     InputOTPSeparator,
     InputOTPSlot,
-} from "@/components/ui/input-otp"
-import { Link } from "react-router"
-import { Info } from "lucide-react"
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
+} from "@/components/ui/input-otp";
+import { Form, FormField, FormItem, FormMessage, FormControl } from "@/components/ui/form";
+import { Link, useNavigate } from "react-router"
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, } from '@tanstack/react-query'
+import { signupUser } from "../../api/user"
+import { toast } from "sonner"
 
-export function SignForm({ className, ...props }: React.ComponentProps<"form">) {
+const formSchema = z.object({
+    userName: z.string().min(3, { message: "Username must be at least 3 characters" }),
+    phoneNumber: z.string().regex(/^\d{10}$/, { message: "Phone number must be 10 digits" }),
+    mPin: z.string().length(6, { message: "MPIN must be exactly 6 digits" }).regex(/^\d+$/, {
+        message: "MPIN must be numeric",
+    }),
+});
+
+export function SignupForm({ className, ...props }: React.ComponentProps<"form">) {
+
+    const navigate = useNavigate()
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            userName: "",
+            phoneNumber: "",
+            mPin: "",
+        },
+    });
+
+
+    const { mutate, isPending } = useMutation({
+        mutationFn: signupUser,
+        onSuccess: (data) => {
+            toast("Signup successful! 🎉");
+            navigate("/login")
+            console.log("User signed up:", data);
+        },
+        onError: (error: Error) => {
+            toast(error.message)
+        }
+    })
+
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+        console.log(values)
+        mutate(values)
+    };
+
     return (
-        <form className={cn("flex flex-col gap-6", className)} {...props}>
-            <div className="flex flex-col items-center gap-2 text-center">
-                <h1 className="text-2xl font-bold">Register your account</h1>
-                <p className="text-muted-foreground text-sm text-balance">
-                    Enter your name and phone number below to register your account
-                </p>
-            </div>
-            <div className="grid gap-6">
-                <div className="grid gap-3">
-                    <Label htmlFor="name">Name</Label>
-                    <Input id="name" type="text" placeholder="eg: Arun Kumar" required />
+        <Form {...form}>
+            <form
+                className={cn("flex flex-col gap-6", className)}
+                {...props}
+                onSubmit={form.handleSubmit(onSubmit)}
+            >
+                <div className="flex flex-col items-center gap-2 text-center">
+                    <h1 className="text-2xl font-bold">Register your account</h1>
+                    <p className="text-muted-foreground text-sm text-balance">
+                        Enter your name & phone number below to register your account
+                    </p>
                 </div>
-                <div className="grid gap-3">
-                    <Label htmlFor="phoneNumber">Phone Number</Label>
-                    <Input id="phoneNumber" type="number" placeholder="1234567890" required />
+                <div className="grid gap-6">
+                    <FormField
+                        control={form.control}
+                        name="userName"
+                        render={({ field }) => (
+                            <FormItem>
+                                <Label htmlFor="userName">Name</Label>
+                                <FormControl>
+                                    <Input id="userName" type="text" placeholder="Arun Kumar" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="phoneNumber"
+                        render={({ field }) => (
+                            <FormItem>
+                                <Label htmlFor="phoneNumber">Phone Number</Label>
+                                <FormControl>
+                                    <Input id="phoneNumber" type="text" placeholder="1234567890" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="mPin"
+                        render={({ field }) => (
+                            <FormItem>
+                                <Label htmlFor="password">M-PIN</Label>
+                                <FormControl>
+                                    <InputOTP maxLength={6} {...field}>
+                                        <InputOTPGroup>
+                                            <InputOTPSlot index={0} />
+                                            <InputOTPSlot index={1} />
+                                            <InputOTPSlot index={2} />
+                                        </InputOTPGroup>
+                                        <InputOTPSeparator />
+                                        <InputOTPGroup>
+                                            <InputOTPSlot index={3} />
+                                            <InputOTPSlot index={4} />
+                                            <InputOTPSlot index={5} />
+                                        </InputOTPGroup>
+                                    </InputOTP>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit" className="w-full">
+                        {isPending ? <span className="flex gap-1 items-center">
+                            <span className="dot w-2 h-2 bg-white rounded-full animate-bounce [animation-delay:-0.2s]"></span>
+                            <span className="dot w-2 h-2 bg-white rounded-full animate-bounce [animation-delay:-0.1s]"></span>
+                            <span className="dot w-2 h-2 bg-white rounded-full animate-bounce"></span>
+                        </span> : "Sign Up"}
+                    </Button>
                 </div>
-                <div className="grid gap-3">
-                    <div className="flex items-center justify-between">
-                        <Label htmlFor="mpin">M-PIN</Label>
-                        <Tooltip>
-                            <TooltipTrigger asChild>
-                                <Info className="h-4 w-4 text-gray-500 cursor-pointer" />
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <p className="text-sm">This MPIN is required for future logins.</p>
-                            </TooltipContent>
-                        </Tooltip>
-                    </div>
-                    <InputOTP maxLength={6}>
-                        <InputOTPGroup>
-                            <InputOTPSlot index={0} />
-                            <InputOTPSlot index={1} />
-                            <InputOTPSlot index={2} />
-                        </InputOTPGroup>
-                        <InputOTPSeparator />
-                        <InputOTPGroup>
-                            <InputOTPSlot index={3} />
-                            <InputOTPSlot index={4} />
-                            <InputOTPSlot index={5} />
-                        </InputOTPGroup>
-                    </InputOTP>
+                <div className="text-center text-sm">
+                    Already have an account?
+                    <Link to="/login" className="ml-1 underline text-blue-600 hover:text-blue-800">
+                        Login
+                    </Link>
                 </div>
-                <Button type="submit" className="w-full">
-                    Sign Up
-                </Button>
-            </div>
-            <div className="text-center text-sm">
-                Already have an account?
-                <Link to="/login" className="ml-1 underline text-blue-600 hover:text-blue-800">
-                    Login
-                </Link>
-            </div>
-        </form>
-    )
+            </form>
+        </Form>
+    );
 }
-

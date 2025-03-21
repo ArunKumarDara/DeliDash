9ca236d9e89ed415203d3de08dd01e54a5fe40c6
Arun Kumar <mymailarun32@gmail.com>
2025-03-21 09:20:02 +0530

    feat(signup): add Signup page with responsive layout
    - Implemented `Signup` page using ShadCN UI components.
    - Integrated `SignupForm` component for user registration.
    - Added responsive design with a two-column layout on large screens.
    - Included brand logo with `CookingPot` icon from Lucide React.
    - Displayed a placeholder image for the right-side background.

--- frontend/src/pages/signup/signup.tsx
+++ frontend/src/pages/signup/signup.tsx
@@ -1,12 +1,13 @@
+import { SignupForm } from "@/components/signup-form"
 import { CookingPot } from "lucide-react"
-import { SignForm } from "@/components/signup-form"
+
 
 export default function Signup() {
     return (
         <div className="grid min-h-svh lg:grid-cols-2">
             <div className="flex flex-col gap-4 p-6 md:p-10">
                 <div className="flex justify-center gap-2 md:justify-start">
-                    <a href="#" className="flex items-center gap-2 font-medium">
+                    <a className="flex items-center gap-2 font-medium">
                         <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
                             <CookingPot className="size-4" />
                         </div>
@@ -15,7 +16,7 @@ export default function Signup() {
                 </div>
                 <div className="flex flex-1 items-center justify-center">
                     <div className="w-full max-w-xs">
-                        <SignForm />
+                        <SignupForm />
                     </div>
                 </div>
             </div>

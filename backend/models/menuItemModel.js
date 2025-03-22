6eda9f6f483d408c61e1d78c3632eb6f43703f63
Arun Kumar <mymailarun32@gmail.com>
2025-03-22 07:22:03 +0530

    feat(menuItemModel): add isSpicy, isVeg, and isBestseller fields to menu item schema
    - Added `isSpicy` field to indicate spiciness of a menu item.
    - Added `isVeg` field to specify if the item is vegetarian.
    - Added `isBestseller` field to mark popular items.

--- backend/models/menuItemModel.js
+++ backend/models/menuItemModel.js
@@ -7,8 +7,11 @@ const menuItemSchema = new mongoose.Schema({
     required: true,
   },
   name: { type: String, required: true },
+  description: { type: String, required: true },
+  isBestseller: { type: Boolean, default: false },
+  isSpicy: { type: Boolean, default: false },
   price: { type: Number, required: true },
-  category: { type: String },
+  isVeg: { type: Boolean, default: true, required: true },
   available: { type: Boolean, default: true },
 });
 

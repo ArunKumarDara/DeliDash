83289e85dba11bd987789778ddaefcce80d0a6c8
Arun Kumar <mymailarun32@gmail.com>
2025-03-22 16:09:47 +0530

    feat(store): wrap application with Redux Provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,8 @@ import Home from "./pages/home/Home";
 import Restaurants from "./pages/restaurants/Restaurants";
 import Dashboard from "./pages/admin/Dashboard";
 import RestaurantMenu from "./pages/restaurants/Menu";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 
 const queryClient = new QueryClient()
@@ -18,25 +20,27 @@ const queryClient = new QueryClient()
 function App() {
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Toaster position="top-right" richColors />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="restaurants" >
-              <Route index element={<Restaurants />} />
-              <Route path=":restaurantId" element={<RestaurantMenu />} /> {/* Dynamic route using 'name' */}
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Toaster position="top-right" richColors />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="restaurants" >
+                <Route index element={<Restaurants />} />
+                <Route path=":restaurantId" element={<RestaurantMenu />} /> {/* Dynamic route using 'name' */}
+              </Route>
+              {/* <Route path="grocery" element={<Grocery />} /> */}
+              {/* <Route path="bakes" element={<Bakes />} /> */}
             </Route>
-            {/* <Route path="grocery" element={<Grocery />} /> */}
-            {/* <Route path="bakes" element={<Bakes />} /> */}
-          </Route>
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Provider>
   )
 }
 

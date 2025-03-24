dafcec89c1e5a64b970f6a32b862c465c56af7f5
Arun Kumar <mymailarun32@gmail.com>
2025-03-24 12:15:41 +0530

    feat(userRouter): add update user route
    - Added `/users/update` route to allow users to update their profile.
    - Implemented authentication middleware to ensure only authorized users can update their details.
    - Integrated `updateUserProfile` controller to handle profile updates.

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -3,6 +3,7 @@ import {
   login,
   signup,
   getUserProfile,
+  updateUserProfile,
 } from "../controllers/userController.js";
 import { authenticateUser } from "../middleware/authMiddleware.js";
 
@@ -11,5 +12,6 @@ const userRouter = express.Router();
 userRouter.post("/signup", signup);
 userRouter.post("/login", login);
 userRouter.get("/profile", authenticateUser, getUserProfile);
+userRouter.post("/update", authenticateUser, updateUserProfile);
 
 export default userRouter;

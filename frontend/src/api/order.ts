4527ed4eea1101e576fcd0881eac89ea02e99061
Arun Kumar <mymailarun32@gmail.com>
2025-03-28 06:55:50 +0530

    feat(api/order): add order placement and retrieval API calls
    - Implement `placeOrder` function to send order details to the backend
    - Implement `getOrderById` function to fetch order details by ID
    - Handle API errors with appropriate error messages

---
+++ frontend/src/api/order.ts
@@ -0,0 +1,91 @@
+import API from "./axiosInstance";
+
+interface OrderResponse {
+  _id: string;
+  addressId: {
+    _id: string;
+    street: string;
+    city: string;
+    zip: string;
+  };
+  deliveryTime: string;
+  deliveryInstructions?: string;
+  menuItems: {
+    item: {
+      _id: string;
+      name: string;
+      price: number;
+    };
+    quantity: number;
+    restaurant: {
+      _id: string;
+      name: string;
+      cuisineType: string;
+    };
+  }[];
+  totalAmount: number;
+  paymentMethod: string;
+}
+
+interface OrderPayload {
+  addressId: string;
+  deliveryTime: string;
+  deliveryInstructions: string;
+  menuItems: {
+    item: {
+      _id: string;
+      name: string;
+      price: number;
+    };
+    quantity: number;
+  }[];
+  totalAmount: number;
+  paymentMethod: string;
+}
+
+interface ApiError {
+  response?: {
+    data?: {
+      message?: string;
+    };
+  };
+  message?: string;
+}
+
+export const placeOrder = async (
+  orderData: OrderPayload
+): Promise<OrderPayload> => {
+  try {
+    const response = await API.post<OrderPayload>("/orders/add", orderData, {
+      withCredentials: true,
+    });
+
+    // Return the created order data
+    return response.data;
+  } catch (error: unknown) {
+    const apiError = error as ApiError;
+    throw new Error(
+      apiError.response?.data?.message ||
+        apiError.message ||
+        "Failed to fetch restaurant details. Please try again."
+    );
+  }
+};
+
+export const getOrderById = async (orderId: string): Promise<OrderResponse> => {
+  try {
+    const response = await API.get<OrderResponse>("/orders/getById", {
+      params: { orderId: orderId },
+      withCredentials: true,
+    });
+
+    return response.data;
+  } catch (error: unknown) {
+    const apiError = error as ApiError;
+    throw new Error(
+      apiError.response?.data?.message ||
+        apiError.message ||
+        "Failed to fetch order details. Please try again."
+    );
+  }
+};

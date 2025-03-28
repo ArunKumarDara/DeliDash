4f41ed887381c9167d1eda4dad181ccea0341f47
Arun Kumar <mymailarun32@gmail.com>
2025-03-28 18:55:06 +0530

    chore(axiosInstance): set default Content-Type header in Axios instance

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 const API = axios.create({
   baseURL: "http://localhost:8080/api/v1",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;

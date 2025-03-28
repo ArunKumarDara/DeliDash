59322234b13a4491ae6038c516327fc9dce43932
Arun Kumar <mymailarun32@gmail.com>
2025-03-28 07:01:52 +0530

    feat(menu): add authwntcate middleware

--- backend/router/menu.js
+++ backend/router/menu.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { addMenuItem, getMenuItems } from "../controllers/menuController.js";
+import { authenticateUser } from "../middleware/authMiddleware.js";
 
 const menuRouter = express.Router();
 
-menuRouter.post("/add", addMenuItem);
-menuRouter.get("/get", getMenuItems);
+menuRouter.post("/add", authenticateUser, addMenuItem);
+menuRouter.get("/get", authenticateUser, getMenuItems);
 
 export default menuRouter;

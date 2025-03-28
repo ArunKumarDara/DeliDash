9246ea152dff0fb0b96be0a694a84472c7389944
Arun Kumar <mymailarun32@gmail.com>
2025-03-28 06:56:22 +0530

    feat(orderRouter): add order routes with authentication
    - Implement routes for adding and retrieving orders
    - Secure routes with `authenticateUser` middleware
    - Include endpoints for fetching all orders and user-specific orders

---
+++ backend/router/orderRouter.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  addOrder,
+  getOrderById,
+  getOrders,
+  getOrdersByUserId,
+} from "../controllers/orderController.js";
+import { authenticateUser } from "../middleware/authMiddleware.js";
+
+const orderRouter = express.Router();
+
+orderRouter.post("/add", authenticateUser, addOrder);
+orderRouter.get("/getById", authenticateUser, getOrderById);
+orderRouter.get("/get", authenticateUser, getOrders);
+orderRouter.get("/getByUserId", authenticateUser, getOrdersByUserId);
+
+export default orderRouter;

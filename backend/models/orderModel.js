d957ba6c6b3c6577d15b5c1457c0767d211c2df9
Arun Kumar <mymailarun32@gmail.com>
2025-03-28 15:49:22 +0530

    refactor(orderModel): move userId to the top of Order schema for better readability

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema(
   {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     addressId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Address",

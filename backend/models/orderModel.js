4c194b330beed6ade240540b84edd4cd8920238c
Arun Kumar <mymailarun32@gmail.com>
2025-03-28 06:59:08 +0530

    feat(orderModel): add Mongoose schema for orders
    - Define `Order` schema with address, menu items, total amount, and payment method
    - Add `status` field with predefined order statuses
    - Enable timestamps for tracking order creation and updates

---
+++ backend/models/orderModel.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    addressId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Address",
+      required: true,
+    },
+    deliveryTime: {
+      type: String,
+      required: true,
+    },
+    deliveryInstructions: {
+      type: String,
+    },
+    menuItems: [
+      {
+        item: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "MenuItem",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+        restaurant: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Restaurant",
+          required: true,
+        },
+      },
+    ],
+    totalAmount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["Cash on Delivery", "Online Payment"],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: [
+        "placed",
+        "confirmed",
+        "preparing",
+        "on the way",
+        "delivered",
+        "cancelled",
+      ],
+      default: "placed",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", orderSchema);

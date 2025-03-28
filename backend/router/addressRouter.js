b8e85f88d0108f993fdf87164ebd0f8542b8b4f6
Arun Kumar <mymailarun32@gmail.com>
2025-03-28 18:54:18 +0530

    feat(addressRouter): add route for setting default address

--- backend/router/addressRouter.js
+++ backend/router/addressRouter.js
@@ -3,6 +3,7 @@ import {
   addAddress,
   deleteAddress,
   getUserAddresses,
+  setDefaultAddress,
   updateAddress,
 } from "../controllers/addressController.js";
 import { authenticateUser } from "../middleware/authMiddleware.js";
@@ -13,5 +14,10 @@ addressRouter.post("/add", authenticateUser, addAddress);
 addressRouter.get("/get", authenticateUser, getUserAddresses);
 addressRouter.post("/update", authenticateUser, updateAddress);
 addressRouter.delete("/delete", authenticateUser, deleteAddress);
+addressRouter.put(
+  "/setDefault/:addressId",
+  authenticateUser,
+  setDefaultAddress
+);
 
 export default addressRouter;

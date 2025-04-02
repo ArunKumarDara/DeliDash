ce34e0f106a9be2da2ac79eb33bc2c551931b3eb
Arun Kumar <mymailarun32@gmail.com>
2025-04-02 08:15:57 +0530

    feat(app0: implement lazy loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,57 +1,100 @@
 import { BrowserRouter, Routes, Route } from "react-router"
-import Login from "./pages/login/login"
-import Signup from "./pages/signup/signup"
+import { lazy, Suspense } from "react"
 import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
 import { Toaster } from "sonner";
-import Layout from "./pages/layout/Layout";
-import Home from "./pages/home/Home";
-import Restaurants from "./pages/restaurants/Restaurants";
-import RestaurantMenu from "./pages/restaurants/Menu";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
-import Checkout from "./pages/checkout/Checkout";
-import Profile from "./pages/profile/Profile";
-// import AdminDashboard from "./pages/admin/dashboard/Page";
-// import AdminDashboard from "./admin/AdminDashboard";
-import Dashboard from "./admin/dashBoard/Dashboard";
-import OrderConfirmation from "./pages/orders/OrderConfirmation";
 import AdminRoute from "./components/admin/Adminroute";
-import Grocery from "./pages/grocery/Grocery";
-import Bakes from "./pages/bakes/Bakes";
-// import Bakery from "./pages/bakes/Bakes";
-// import AdminRestaurants from "./admin/restaurants/Restaurants";
+import Layout from "./pages/layout/Layout";
+import Loading from "./components/spinner/Loader";
 
+// Lazy-loaded components
+const Login = lazy(() => import("./pages/login/login"));
+const Signup = lazy(() => import("./pages/signup/signup"));
+const Home = lazy(() => import("./pages/home/Home"));
+const Restaurants = lazy(() => import("./pages/restaurants/Restaurants"));
+const RestaurantMenu = lazy(() => import("./pages/restaurants/Menu"));
+const Checkout = lazy(() => import("./pages/checkout/Checkout"));
+const Profile = lazy(() => import("./pages/profile/Profile"));
+const Dashboard = lazy(() => import("./admin/dashBoard/Dashboard"));
+const OrderConfirmation = lazy(() => import("./pages/orders/OrderConfirmation"));
+const Grocery = lazy(() => import("./pages/grocery/Grocery"));
+const Bakes = lazy(() => import("./pages/bakes/Bakes"));
 
 const queryClient = new QueryClient()
 
 function App() {
-
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <Toaster position="top-right" richColors />
         <BrowserRouter>
           <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={
+              <Suspense fallback={<Loading />}>
+                <Login />
+              </Suspense>
+            } />
+            <Route path="/signup" element={
+              <Suspense fallback={<Loading />}>
+                <Signup />
+              </Suspense>
+            } />
+
             <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="restaurants" >
-                <Route index element={<Restaurants />} />
-                <Route path=":restaurantId" element={<RestaurantMenu />} />
+              <Route index element={
+                <Suspense fallback={<Loading />}>
+                  <Home />
+                </Suspense>
+              } />
+              <Route path="restaurants">
+                <Route index element={
+                  <Suspense fallback={<Loading />}>
+                    <Restaurants />
+                  </Suspense>
+                } />
+                <Route path=":restaurantId" element={
+                  <Suspense fallback={<Loading />}>
+                    <RestaurantMenu />
+                  </Suspense>
+                } />
               </Route>
-              <Route path="grocery" element={<Grocery />} />
-              <Route path="bakes" element={<Bakes />} />
-              <Route path="checkout" element={<Checkout />} />
-              <Route path="profile" element={<Profile />} />
-              {/* <Route path="admin" element={<Dashboard />} /> */}
-              <Route path="order-confirmation/:orderId" element={<OrderConfirmation />} />
+              <Route path="grocery" element={
+                <Suspense fallback={<Loading />}>
+                  <Grocery />
+                </Suspense>
+              } />
+              <Route path="bakes" element={
+                <Suspense fallback={<Loading />}>
+                  <Bakes />
+                </Suspense>
+              } />
+              <Route path="checkout" element={
+                <Suspense fallback={<Loading />}>
+                  <Checkout />
+                </Suspense>
+              } />
+              <Route path="profile" element={
+                <Suspense fallback={<Loading />}>
+                  <Profile />
+                </Suspense>
+              } />
+              <Route path="order-confirmation/:orderId" element={
+                <Suspense fallback={<Loading />}>
+                  <OrderConfirmation />
+                </Suspense>
+              } />
             </Route>
+
             <Route path="/admin" element={<AdminRoute />}>
-              <Route index element={<Dashboard />} />
+              <Route index element={
+                <Suspense fallback={<Loading />}>
+                  <Dashboard />
+                </Suspense>
+              } />
             </Route>
           </Routes>
         </BrowserRouter>

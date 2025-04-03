8549517ead2617a020de64b3eb473ea762f2687d
DARA ARUN KUMAR <mymailarun32@gmail.com>
2025-04-03 21:49:09 +0530

    Update axiosInstance.ts

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "http://3.7.31.1:8080/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",

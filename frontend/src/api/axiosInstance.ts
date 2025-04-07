1e3e6a7dfc1c142aba92200fb7903c7364d4076e
Arun Kumar <mymailarun32@gmail.com>
2025-04-07 16:11:44 +0530

    refactor: change base url

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:8080/api/v1",
-  baseURL: "http://52.66.152.76/api/v1",
+  baseURL: "http://3.6.240.135:8080/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",

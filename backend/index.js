4de1ba2cdbfc698e0a73027d0ad83696f111a26f
Arun Kumar <mymailarun32@gmail.com>
2025-04-07 22:29:45 +0530

    refactor: change base urls

--- backend/index.js
+++ backend/index.js
@@ -24,11 +24,6 @@ const port = process.env.PORT || 5000;
   app.use(
     cors({
       // origin: "http://localhost:5173",
-      // origin:
-      //   "http://3.6.240.135:3000" ||
-      //   "http://localhost:3000" ||
-      //   "http://0.0.0.0:3000",
-      // origin: "*",
       origin: "http://3.6.240.135:3000",
       // origin: "*",
       credentials: true,
--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:8080/api/v1",
+
   baseURL: "http://3.6.240.135:8080/api/v1",
   withCredentials: true,
   headers: {

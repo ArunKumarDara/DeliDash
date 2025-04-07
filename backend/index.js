f8b1fbeffbe61bac7c0a954e945553daa56d504f
Arun Kumar <mymailarun32@gmail.com>
2025-04-07 17:10:49 +0530

    refactor: change cors methods

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,10 @@ const port = process.env.PORT || 5000;
   app.use(
     cors({
       // origin: "http://localhost:5173",
-      origin: "http://3.6.240.135:3000" || "http://localhost:3000",
+      origin:
+        "http://3.6.240.135:3000" ||
+        "http://localhost:3000" ||
+        "http://0.0.0.0:3000",
       // origin: "*",
       credentials: true,
       // httpOnly: true,

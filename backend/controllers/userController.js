03504d533820ca33c2cba1113dd3d3d42afb6986
Arun Kumar <mymailarun32@gmail.com>
2025-04-07 17:18:54 +0530

    refactor: change cookie policies

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -75,7 +75,7 @@ export const login = async (req, res) => {
 
     res.cookie("authToken", token, {
       httpOnly: true, // Prevent XSS attacks
-      secure: process.env.NODE_ENV === "production", // Only send in HTTPS (Production)
+      // secure: process.env.NODE_ENV === "production",
       sameSite: "Strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });

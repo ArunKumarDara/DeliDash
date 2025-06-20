5955353fb7ded468c1eb2c4b68dc2cd8765a06a0
Arun Kumar <mymailarun32@gmail.com>
2025-06-20 08:47:41 +0530

    chore: change cookie permissions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,8 +74,8 @@ export const login = async (req, res) => {
     );
 
     res.cookie("authToken", token, {
-      // httpOnly: true,
-      // secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
       sameSite: "Strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
